05b98600609b441c841d9e5af6ac1bd222c2cecb
test_nas <zkunsang@gmail.com>
2021-02-23 11:53:00 +0900

    userQuestStory

--- node_storyself/models/mongo/UserQuestStory.js
+++ node_storyself/models/mongo/UserQuestStory.js
@@ -7,29 +7,38 @@ const Schema = {
     _ID: { key: '_id', required: false, type: ValidType.OBJECT },
     UID: { key: 'uid', required: true, type: ValidType.STRING },
     STORY_ID: { key: 'storyId', required: true, type: ValidType.STRING },
-    QUEST_STORY: { key: 'questStory', required: true, type: ValidType.OBJECT },
-    STORY_ACTION: { key: 'storyAction', required: true, type: ValidType.OBJECT },
+    QUEST_CLEAR: { key: 'questClear', required: false, type: ValidType.OBJECT },
+    QUEST_ACCEPT: { key: 'questAccept', required: false, type: ValidType.OBJECT },
+    QUEST_ACTION: { key: 'storyAction', required: false, type: ValidType.OBJECT },
 }
 
 class UserQuestStory extends Model {
-    constructor({ _id, uid, storyId, questStory, storyAction }) {
+    constructor({ _id, uid, storyId, questClear, questAccept, storyAction }) {
         super();
 
         this[Schema._ID.key] = _id;
         this[Schema.UID.key] = ValidateUtil.setNullUndefined(uid);
-        this[Schema.STORY_ID.key] = storyId
-        this[Schema.QUEST_STORY.key] = questStory;
-        this[Schema.STORY_ACTION.key] = storyAction;
-    }
-
-    getQuestStory() {
-        return this[Schema.QUEST_STORY.key];
+        this[Schema.STORY_ID.key] = storyId;
+        this[Schema.QUEST_CLEAR.key] = questClear;
+        this[Schema.QUEST_ACCEPT.key] = questAccept;
+        this[Schema.QUEST_ACTION.key] = storyAction;
     }
 
     getStoryAction() {
-        return this[Schema.STORY_ACTION.key];
+        return this[Schema.QUEST_ACTION.key] || {};
     }
 
+    getQuestClear() {
+        return this[Schema.QUEST_CLEAR.key] || {};
+    }
+
+    getQuestAccept() {
+        return this[Schema.QUEST_ACCEPT.key] || {};
+    }
+
+    getObjectId() {
+        return this[Schema._ID.key];
+    }
 };
 
 module.exports = UserQuestStory;
