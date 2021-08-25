e78ae0e12f13e6f271a71976e62fc7313539344f
zkunsang <zkunsang@gmail.com>
2021-08-25 22:48:45 +0900

    inventory login update ended

---
+++ scripts/storyMerge.js
@@ -0,0 +1,79 @@
+// kr, en을 book으로 합칠때 잘못된 아이템 아이디 수정
+
+process.on('uncaughtException', (err) => console.error(err));
+require('./startup');
+
+const moment = require("moment");
+
+(async () => {
+    try {
+        await require('./boot/initSS')();
+        await test();
+    } catch (error) {
+        console.error(error);
+        process.exit();
+    }
+})();
+
+
+const dbMongo = require('@ss/dbMongo');
+const UserDao = require('@ss/daoMongo/UserDao');
+const User = require('@ss/models/mongo/User');
+const InventoryService = require("@ss/service/InventoryService");
+const UserService = require("@ss/service/UserService");
+const StoryCache = require('@ss/dbCache/StoryCache');
+const ItemCache = require('@ss/dbCache/ItemCache');
+
+
+// 스토리 아이디 가져오기
+
+const storyMapping = {
+    "Goldilocks": "Goldilocks_kr",
+    "PussInBoots": "PussInBoots_kr",
+    "sunandmoon": "sunandmoon_kr",
+    "pinocchio": "pinocchio_kr",
+    "nutcracker": "nutcracker_kr",
+    "TheLittlePrince": "TheLittlePrince_kr",
+    "PeterPan": "PeterPan_kr",
+    "AliceInWonderland": "AliceInWonderland_kr",
+    "snowsoup": "snowsoup_kr",
+}
+// 일반 스토리 kr로 변경
+async function test() {
+    const updateDate = moment().valueOf();
+    const storyList = StoryCache.getList();
+
+    // 스토리 아이디 가져오기
+    const userDao = new UserDao(dbMongo);
+    const userList = await userDao.findAll();
+    for (const user of userList) {
+        const { uid, inventory } = user;
+
+        const newInventory = [];
+        let needUpdate = false;
+        for (let i = 0; i < inventory.length; i++) {
+            if (inventory[i].itemId.includes('Book')) {
+                needUpdate = true;
+                continue;
+            }
+            newInventory.push(inventory[i]);
+        }
+
+        if (!needUpdate) continue;
+
+        const userService = new UserService(user, userDao, updateDate);
+        userService.setInventory(newInventory);
+
+        await userService.finalize();
+    }
+}
+
+
+
+
+
+
+
+
+
+
