9dd8a57e35afb72937b61ae5f82ed19d63c21d39
zkunsang <zkunsang@gmail.com>
2021-10-21 11:39:51 +0900

    sendmail

---
+++ scripts/sendMail.js
@@ -0,0 +1,65 @@
+// 리딩 타운 가격인하로
+// 꿀 환불
+process.on("uncaughtException", (err) => console.error(err));
+require("./startup");
+
+const moment = require("moment");
+
+(async () => {
+  try {
+    await require("./boot/initSS")();
+    await test();
+  } catch (error) {
+    console.error(error);
+    process.exit();
+  }
+})();
+
+const dbMongo = require("@ss/dbMongo");
+const UserDao = require("@ss/daoMongo/UserDao");
+const User = require("@ss/models/mongo/User");
+const InventoryService = require("@ss/service/InventoryService");
+const UserService = require("@ss/service/UserService");
+const MailService = require("@ss/service/MailService");
+const { MailSender } = require("@ss/util/ValidateUtil");
+
+const readingTownTale = ["TheCreepyCaterpillar_en", "CupcakesforGrandma_en"];
+
+// 일반 스토리 kr로 변경
+async function test() {
+  const updateDate = moment().valueOf();
+  const userDao = new UserDao(dbMongo);
+  const user = await userDao.findOne({ uid: "9214" });
+
+  const userInfo = new User(user);
+  const mailService = new MailService(userInfo, updateDate);
+
+  const itemInfo = {
+    action: InventoryService.PUT_ACTION.ADMIN,
+  };
+
+  const title = "스토리 셀프 240꿀을 드립니다.";
+  const message = `스토리 셀프를 구매해 주셔서 감사합니다`;
+  const title_en = "스토리 셀프 240꿀을 드립니다.";
+  const message_en = `스토리 셀프를 구매해 주셔서 감사합니다`;
+
+  const sender = MailSender.ADMIN;
+  const itemList = [{ itemId: "honey", itemQny: 240 }];
+
+  // const senderId = ''
+  mailService.sendMail({
+    title,
+    title_en,
+    message,
+    message_en,
+    sender,
+    itemList,
+    itemInfo,
+  });
+  const newMail = mailService.finalize();
+
+  const userService = new UserService(userInfo, userDao, updateDate);
+  userService.setMail(newMail);
+
+  await userService.finalize();
+}
