6b98441e8f24b3d524de496d2bd663ea422b8547
zkunsang <zkunsang@gmail.com>
2021-12-07 23:41:23 +0900

    story column updated

--- node_storyself/models/mongo/Story.js
+++ node_storyself/models/mongo/Story.js
@@ -1,98 +1,146 @@
-const Model = require('../../models');
+const Model = require("../../models");
 
-const ValidateUtil = require('../../util/ValidateUtil')
+const ValidateUtil = require("../../util/ValidateUtil");
 const ValidType = ValidateUtil.ValidType;
 const StoryStatus = ValidateUtil.StoryStatus;
 
 const Schema = {
-    STORY_ID: { key: 'storyId', required: true, type: ValidType.STRING },
-    STATUS: { key: 'status', required: true, type: ValidType.NUMBER, validRange: Object.values(StoryStatus) },
-    VERSION: { key: 'version', required: true, type: ValidType.NUMBER },
-    UPDATE_DATE: { key: 'updateDate', required: true, type: ValidType.UNIX_TIMESTAMP },
-    THUMBNAIL: { key: 'thumbnail', required: false, type: ValidType.STRING },
-    THUMBNAIL_CRC32: { key: 'thumbnailCrc32', required: false, type: ValidType.STRING },
-    THUMBNAIL_VERSION: { key: 'thumbnailVersion', required: false, type: ValidType.NUMBER },
-    TEXTFILE: { key: 'textFile', required: false, type: ValidType.STRING },
-    TEXTFILE_CRC32: { key: 'textFileCrc32', required: false, type: ValidType.STRING },
-    TEXTFILE_VERSION: { key: 'textFileVersion', required: false, type: ValidType.NUMBER },
+  STORY_ID: { key: "storyId", required: true, type: ValidType.STRING },
+  STATUS: {
+    key: "status",
+    required: true,
+    type: ValidType.NUMBER,
+    validRange: Object.values(StoryStatus),
+  },
+  VERSION: { key: "version", required: true, type: ValidType.NUMBER },
+  UPDATE_DATE: {
+    key: "updateDate",
+    required: true,
+    type: ValidType.UNIX_TIMESTAMP,
+  },
+  THUMBNAIL: { key: "thumbnail", required: false, type: ValidType.STRING },
+  THUMBNAIL_CRC32: {
+    key: "thumbnailCrc32",
+    required: false,
+    type: ValidType.STRING,
+  },
+  THUMBNAIL_VERSION: {
+    key: "thumbnailVersion",
+    required: false,
+    type: ValidType.NUMBER,
+  },
+  TEXTFILE: { key: "textFile", required: false, type: ValidType.STRING },
+  TEXTFILE_CRC32: {
+    key: "textFileCrc32",
+    required: false,
+    type: ValidType.STRING,
+  },
+  TEXTFILE_VERSION: {
+    key: "textFileVersion",
+    required: false,
+    type: ValidType.NUMBER,
+  },
 
-    FACE_TAG: { key: 'faceTag', required: false, type: ValidType.STRING },
+  FACE_TAG: { key: "faceTag", required: false, type: ValidType.STRING },
 
-    LANG: { key: 'lang', required: false, type: ValidType.NUMBER },
-    PUBLISHER: { key: 'publisher', required: false, type: ValidType.NUMBER },
-    EXPOSE: { key: 'expose', required: false, type: ValidType.NUMBER },
-}
+  LANG: { key: "lang", required: false, type: ValidType.NUMBER },
+  PUBLISHER: { key: "publisher", required: false, type: ValidType.NUMBER },
+  EXPOSE: { key: "expose", required: false, type: ValidType.NUMBER },
+
+  RECORD_COUNT: { key: "recordCount", required: false, type: ValidType.NUMBER },
+};
 
 class Story extends Model {
-    constructor({ storyId, status, version, thumbnail, thumbnailCrc32, thumbnailVersion, textFile, textFileVersion, textFileCrc32, faceTag, lang, publisher, expose }) {
-        super();
+  constructor({
+    storyId,
+    status,
+    version,
+    thumbnail,
+    thumbnailCrc32,
+    thumbnailVersion,
+    textFile,
+    textFileVersion,
+    textFileCrc32,
+    faceTag,
+    lang,
+    publisher,
+    expose,
+    recordCount,
+  }) {
+    super();
 
-        this[Schema.STORY_ID.key] = ValidateUtil.setNullUndefined(storyId);
-        this[Schema.STATUS.key] = ValidateUtil.setNullUndefined(status);
-        this[Schema.VERSION.key] = ValidateUtil.setNullUndefined(version);
+    this[Schema.STORY_ID.key] = ValidateUtil.setNullUndefined(storyId);
+    this[Schema.STATUS.key] = ValidateUtil.setNullUndefined(status);
+    this[Schema.VERSION.key] = ValidateUtil.setNullUndefined(version);
 
-        this[Schema.THUMBNAIL.key] = ValidateUtil.setNullUndefined(thumbnail);
-        this[Schema.THUMBNAIL_CRC32.key] = ValidateUtil.setNullUndefined(thumbnailCrc32);
-        this[Schema.THUMBNAIL_VERSION.key] = ValidateUtil.setNullUndefined(thumbnailVersion);
+    this[Schema.THUMBNAIL.key] = ValidateUtil.setNullUndefined(thumbnail);
+    this[Schema.THUMBNAIL_CRC32.key] =
+      ValidateUtil.setNullUndefined(thumbnailCrc32);
+    this[Schema.THUMBNAIL_VERSION.key] =
+      ValidateUtil.setNullUndefined(thumbnailVersion);
 
-        this[Schema.TEXTFILE.key] = ValidateUtil.setNullUndefined(textFile);
-        this[Schema.TEXTFILE_CRC32.key] = ValidateUtil.setNullUndefined(textFileCrc32);
-        this[Schema.TEXTFILE_VERSION.key] = ValidateUtil.setNullUndefined(textFileVersion);
+    this[Schema.TEXTFILE.key] = ValidateUtil.setNullUndefined(textFile);
+    this[Schema.TEXTFILE_CRC32.key] =
+      ValidateUtil.setNullUndefined(textFileCrc32);
+    this[Schema.TEXTFILE_VERSION.key] =
+      ValidateUtil.setNullUndefined(textFileVersion);
 
-        this[Schema.FACE_TAG.key] = ValidateUtil.setNullUndefined(faceTag);
+    this[Schema.FACE_TAG.key] = ValidateUtil.setNullUndefined(faceTag);
 
-        this[Schema.LANG.key] = ValidateUtil.setNullUndefined(lang);
-        this[Schema.PUBLISHER.key] = ValidateUtil.setNullUndefined(publisher);
-        this[Schema.EXPOSE.key] = ValidateUtil.setNullUndefined(expose);
-    }
+    this[Schema.LANG.key] = ValidateUtil.setNullUndefined(lang);
+    this[Schema.PUBLISHER.key] = ValidateUtil.setNullUndefined(publisher);
+    this[Schema.EXPOSE.key] = ValidateUtil.setNullUndefined(expose);
 
-    setVersion(version) {
-        this[Schema.VERSION.key] = version;
-    }
+    this[Schema.RECORD_COUNT.key] = ValidateUtil.setNullUndefined(recordCount);
+  }
 
-    setUpdateDate(updateDate) {
-        this[Schema.UPDATE_DATE.key] = updateDate;
-    }
+  setVersion(version) {
+    this[Schema.VERSION.key] = version;
+  }
 
-    getStoryId() {
-        return this[Schema.STORY_ID.key];
-    }
+  setUpdateDate(updateDate) {
+    this[Schema.UPDATE_DATE.key] = updateDate;
+  }
 
-    getVersion() {
-        return this[Schema.VERSION.key];
-    }
+  getStoryId() {
+    return this[Schema.STORY_ID.key];
+  }
 
-    getStatus() {
-        return this[Schema.STATUS.key];
-    }
+  getVersion() {
+    return this[Schema.VERSION.key];
+  }
 
-    getThumbnail() {
-        return this[Schema.THUMBNAIL.key];
-    }
+  getStatus() {
+    return this[Schema.STATUS.key];
+  }
 
-    getThumbnailVersion() {
-        return this[Schema.THUMBNAIL_VERSION.key];
-    }
+  getThumbnail() {
+    return this[Schema.THUMBNAIL.key];
+  }
 
-    getThumbnailCrc32() {
-        return this[Schema.THUMBNAIL_CRC32.key];
-    }
+  getThumbnailVersion() {
+    return this[Schema.THUMBNAIL_VERSION.key];
+  }
 
-    getTextFile() {
-        return this[Schema.TEXTFILE.key];
-    }
+  getThumbnailCrc32() {
+    return this[Schema.THUMBNAIL_CRC32.key];
+  }
 
-    getTextFileVersion() {
-        return this[Schema.TEXTFILE_VERSION.key];
-    }
+  getTextFile() {
+    return this[Schema.TEXTFILE.key];
+  }
 
-    getTextFileCrc32() {
-        return this[Schema.TEXTFILE_CRC32.key];
-    }
+  getTextFileVersion() {
+    return this[Schema.TEXTFILE_VERSION.key];
+  }
 
-    getFaceTag() {
-        return this[Schema.FACE_TAG.key];
-    }
+  getTextFileCrc32() {
+    return this[Schema.TEXTFILE_CRC32.key];
+  }
+
+  getFaceTag() {
+    return this[Schema.FACE_TAG.key];
+  }
 }
 
 module.exports = Story;
