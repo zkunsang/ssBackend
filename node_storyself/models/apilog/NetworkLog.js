4ab89226d42bec7ceddb0f079d7c2c3aced1d765
zkunsang <zkunsang@gmail.com>
2021-12-10 13:15:05 +0900

    ms added

--- node_storyself/models/apilog/NetworkLog.js
+++ node_storyself/models/apilog/NetworkLog.js
@@ -5,29 +5,30 @@ const Model = require('../')
 const Schema = {}
 
 class NetworkLog extends Model {
-    constructor(ctx, startDate, endDate) {
-        super();
-        this.pathname = ctx.path;
-        this.ip = ctx.$req.clientIp;
+  constructor(ctx, startDate, endDate) {
+    super();
+    this.pathname = ctx.path;
+    this.ip = ctx.$req.clientIp;
 
-        const body = ctx.request.body;
-        this.deviceId = body.deviceId;
-        this.deviceName = body.deviceName;
-        this.osVersion = body.osVersion;
-        this.gameLanguage = body.gameLanguage;
+    const body = ctx.request.body;
+    this.deviceId = body.deviceId;
+    this.deviceName = body.deviceName;
+    this.osVersion = body.osVersion;
+    this.gameLanguage = body.gameLanguage;
 
-        this.body = JSON.stringify(ctx.request.body);
-        // this.body = ctx.request.body;
-        this.res = ctx.body;
-        this.status = ctx.status;
-        this.ms = endDate - startDate;
-        this.endDate = DateUtil.utsToDs(endDate);
-        this.startDate = DateUtil.utsToDs(startDate);
+    this.body = JSON.stringify(ctx.request.body);
+    // this.body = ctx.request.body;
+    this.res = ctx.body;
+    this.status = ctx.status;
+    this.ms = endDate - startDate;
+    this.ctx.body.common.ms = this.ms;
+    this.endDate = DateUtil.utsToDs(endDate);
+    this.startDate = DateUtil.utsToDs(startDate);
 
-        if(ctx.$userInfo) {
-            this.uid = ctx.$userInfo.getUID();
-        }
+    if (ctx.$userInfo) {
+      this.uid = ctx.$userInfo.getUID();
     }
+  }
 }
 
 module.exports = NetworkLog;
