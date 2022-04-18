274412fbe9f7640f68984f9a4317a38c7c05caca
zkunsang <zkunsang@gmail.com>
2022-04-18 13:02:35 +0900

    receipt 20237

--- scripts/revokereceipt20237.js
+++ scripts/revokereceipt20237.js
@@ -22,7 +22,7 @@ const UserService = require("@ss/service/UserService");
 const ReceiptDao = require('@ss/daoMongo/ReceiptDao');
 const SubscribeReceiptDao = require('@ss/daoMongo/SubscribeReceiptDao');
 const ProductService = require('@ss/service/ProductService');
-const receiptData = require("../scripts/revokereceipt20243.json");
+const receiptData = require("../scripts/revokereceipt20237.json");
 
 // 일반 스토리 kr로 변경
 async function test() {
@@ -30,7 +30,7 @@ async function test() {
   const updateDate = moment().valueOf();
   const userDao = new UserDao(dbMongo);
 
-  const user = await userDao.findOne({ uid: "20243" });
+  const user = await userDao.findOne({ uid: "20237" });
 
   console.log(user.uid, user.subscribeInfo);
 
